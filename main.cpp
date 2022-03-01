18728da8060cae1f3fd316bac74f1c20646db224
tekerinkin <dmshestakov@icloud.com>
2022-03-01 12:09:30 +0300

    Add benchmarks

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include "sorts/sorts.h"
+#include "test/benchmark.h"
 #include <vector>
 #include <iostream>
 
@@ -8,10 +9,9 @@ int main() {
 
     vector<int> v = {2, 3, 1, 5, 4};
 
-    Sorts::selectionSort(v.begin(), v.end());
-
-    for(auto i : v)
-        cout << i << " ";
+    test_two_sorts<vector<int>::iterator, int>(Sorts::insertionSort<vector<int>::iterator>,
+                                            Sorts::bubbleSort<vector<int>::iterator>,
+                                            "insertion_sort", "bubble_sort",10);
 
     return 0;
 }
